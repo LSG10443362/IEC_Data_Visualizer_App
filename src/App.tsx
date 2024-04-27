c81c42c0c63a45741edaaf5f3a3920bbdec40aca
Lesego Sindani <st10443362@vcconnect.edu.za>
2024-04-27 19:24:43 +0200

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './donut.png';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Haha look a donut
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hi cht remember to study for plrd
         </a>
       </header>
     </div>
